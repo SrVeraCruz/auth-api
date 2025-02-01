24d2b3c7cf6f07b91e096905793b4472f1b0a33d
SrVeraCruz <veracruzdudu@gmail.com>
2025-02-01 19:45:40 +0100

    feat: initial commit

---
+++ src/test/java/com/veracruz/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veracruz.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
